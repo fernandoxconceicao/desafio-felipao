1de320d2906fbf756f73bf3b751dca4bdbd09b79
Fernando Conceição <94488257+NandroxxProg@users.noreply.github.com>
2023-11-20 16:17:51 -0300

    adição do código de desafio

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-ksadljsakld;
+let nomeHeroi = "zeldinha"
+let nivelHeroi = 6001
+
+if (nivelHeroi <= 1000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro")
+} else if (nivelHeroi <= 2000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze")
+} else if (nivelHeroi <= 5000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata")
+} else if (nivelHeroi <= 7000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro")
+} else if (nivelHeroi <= 8000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina")
+} else if (nivelHeroi <= 9000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente")
+} else if (nivelHeroi <= 10000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal")
+} else if (nivelHeroi > 10000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante")
+} else {
+    console.log("O seu nível é radiante!")
+}
